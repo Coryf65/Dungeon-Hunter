e48e6d30645ee7cf538b9d7104f7265c2252da69
Cory Fabian <Coryf65@users.noreply.github.com>
2022-10-25 16:38:27 -0500

    added controller to player

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
-    private Rigidbody2D rigidBody2D;
+    private Rigidbody2D _rigidBody2D;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidBody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    // Character Movement
+    // 2D
+    void HandleMovement()
+    {
+        Vector2 movement = new( x: Input.GetAxis("Horizontal"), y: Input.GetAxis("Vertical"));
+
+        _rigidBody2D.MovePosition(_rigidBody2D.position + movement * Time.deltaTime);
     }
 }

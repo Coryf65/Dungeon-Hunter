0c1b80e3806be1624ac155d43e8e05093b58ce8f
Cory Fabian <Coryf65@users.noreply.github.com>
2022-10-28 22:15:46 -0500

    added a sprite flopping code

---
+++ Assets/Scripts/SpriteFlip.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteFlip : CharacterComponent
+{
+    public enum FlipMode
+    {
+        MovementDirection,
+        WeaponDirection
+    }
+
+    [SerializeField] private FlipMode _flipMode = FlipMode.MovementDirection;
+    [SerializeField] private float _threshold = 0.1f;
+
+    protected override void HandleAbility()
+    {
+        base.HandleAbility();
+
+        if (_flipMode == FlipMode.MovementDirection)
+        {
+            FlipToMovementDirection();
+        }
+
+        if (_flipMode == FlipMode.WeaponDirection)
+        {
+            FlipToWeaponDirection();
+        }
+    }
+
+    private void FlipToMovementDirection()
+    {
+        // we are moving
+        if (controller.CurrentMovement.normalized.magnitude > _threshold)
+        {
+            if (controller.CurrentMovement.normalized.x > 0)
+            {
+                FaceDirection(1);
+            } else
+            {
+                FaceDirection(-1);
+            }
+        }
+    }
+
+    private void FlipToWeaponDirection()
+    {
+
+    }
+
+    private void FaceDirection(int newDirection)
+    {
+        // moving right
+        if (newDirection == 1)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        if (newDirection <= 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+}

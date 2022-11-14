62d993470c0e8afcd0d7fa162b5152750c6bfda1
Cory Fabian <Coryf65@users.noreply.github.com>
2022-11-14 14:11:26 -0600

    displaying health value as text

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -6,7 +6,7 @@ public class UIManager : Singleton<UIManager>
 {
     [Header("Settings")]
     [SerializeField] private Image _healthBar;
-    [SerializeField] private TextMeshProUGUI healthValuetext;
+    [SerializeField] private TextMeshProUGUI _healthValueText;
 
     private float _currentHealth;
     private float _maxHealth;
@@ -25,5 +25,7 @@ public class UIManager : Singleton<UIManager>
     private void UpdateUI()
     {
         _healthBar.fillAmount = Mathf.Lerp(_healthBar.fillAmount, _currentHealth / _maxHealth, 10f * Time.deltaTime);
+        // display health like 10/10
+        _healthValueText.text = $"{_currentHealth} / {_maxHealth}";
     }
 }

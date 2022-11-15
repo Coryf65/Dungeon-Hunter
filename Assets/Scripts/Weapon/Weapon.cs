6f4a245ca832cc24f52e53906551d0ccb7ec2893
Cory Fabian <Coryf65@users.noreply.github.com>
2022-11-15 14:09:05 -0600

    using shooting logic

--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -4,7 +4,7 @@ public class Weapon : MonoBehaviour
 {
     [Header("settings")]
     [Tooltip("The Time between shots. How long between each shot will take.")]
-    [SerializeField] private float fireRate = 0.5f;
+    [SerializeField] private float _fireRate = 0.5f;
 
     [Header("Weapon")]
     [SerializeField] private bool _HasMagazine = false;
@@ -16,6 +16,9 @@ public class Weapon : MonoBehaviour
     public int CurrentAmmo { get; set; }
     public bool UseMagazine => _HasMagazine;
     public int MagazineSize => _magazineSize;
+    public bool CanShoot { get; set; }
+
+    private float _nextShotTime;
 
     private void Awake()
     {
@@ -29,14 +32,54 @@ public class Weapon : MonoBehaviour
 
     }
 
+    void Update()
+    {
+        WeaponCanShoot();
+    }
+
+    /// <summary>
+    /// Trigger the weapn to shoot
+    /// </summary>
     public void WeaponShot()
     {
         HandleShooting();
     }
 
+    /// <summary>
+    /// Handles how the weapon shoots
+    /// </summary>
     private void HandleShooting()
     {
-        WeaponAmmo.ConsumeAmmo();
+        if (UseMagazine)
+        {
+            if (WeaponAmmo != null)
+            {
+                if (WeaponAmmo.CanUseWeapon())
+                {
+                    RequestShot();
+                }
+                else
+                {
+                    if (_HasAutoReload)
+                    {
+                        Reload();
+                    }
+                }
+            }
+        }
+        else
+        {
+            RequestShot();
+        }
+    }
+
+    private void WeaponCanShoot()
+    {
+        if (Time.time > _nextShotTime)
+        {
+            CanShoot = true;
+            _nextShotTime = Time.time + _fireRate; // the current time + our time between shots
+        }
     }
 
     public void SetOwner(Character owner)
@@ -46,6 +89,17 @@ public class Weapon : MonoBehaviour
 
     public void Reload()
     {
-        WeaponAmmo.RefillAmmo();
+        if (WeaponAmmo != null)
+        {
+            if (UseMagazine)
+            {
+                WeaponAmmo.RefillAmmo();
+            }
+        }
+    }
+
+    private void RequestShot()
+    {
+        WeaponAmmo.ConsumeAmmo();
     }
 }

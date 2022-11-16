57c90f487f0fd52adb28c393a4e27d6ace96ccc9
Cory Fabian <Coryf65@users.noreply.github.com>
2022-11-16 16:38:09 -0600

    created Camera Shake

--- Assets/Scripts/Camera/Camera2D.cs
+++ Assets/Scripts/Camera/Camera2D.cs
@@ -9,6 +9,10 @@ public class Camera2D : MonoBehaviour
     [Header("Camera Settings")]
     [SerializeField] private Vector2 _offset = new(x: 0, y: 0); // default to center
     [SerializeField] private CameraMode _cameraMode = CameraMode.Update;
+    [SerializeField] private bool _UseCameraShake = false;
+    [SerializeField] private float _shakeVibrato = 10f;
+    [SerializeField] private float _shakeRandomness = 0.1f;
+    [SerializeField] private float _shakeDuration = 0.01f;
     
     private enum CameraMode
     {
@@ -50,5 +54,27 @@ public class Camera2D : MonoBehaviour
         );
 
         transform.position = desiredPosition;
+    }
+
+    public void ShakeCamera()
+    {
+        StartCoroutine(IShaker());
+    }
+
+    /// <summary>
+    /// Coroutine to shake the camera
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator IShaker()
+    {
+        Vector3 currentPosition = transform.position;
+
+        for (int i = 0; i < _shakeVibrato; i++)
+        {
+            Vector3 shakePosition = currentPosition + Random.onUnitSphere * _shakeRandomness;
+            yield return new WaitForSeconds(_shakeDuration);
+            transform.position = shakePosition;
+        }
+        yield return null;
     }
 }

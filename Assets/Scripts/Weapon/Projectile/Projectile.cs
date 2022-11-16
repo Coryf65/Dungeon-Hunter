e3a1a659fa4e36f5c3a2b71cabf9e54869957222
Cory Fabian <Coryf65@users.noreply.github.com>
2022-11-16 14:59:02 -0600

    Creating a projectile script

--- Assets/Scripts/Weapon/Projectile/Projectile.cs
+++ Assets/Scripts/Weapon/Projectile/Projectile.cs
@@ -1,18 +1,61 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    [SerializeField] private float _speed = 100f;
+    [SerializeField] private float _acceleration = 0f;
+
+    private Rigidbody2D _rigidbody2D;
+    private SpriteRenderer _spriteRenderer;
+    private Vector2 _movement;
+
+    public Vector2 Direction { get; set; }
+    public bool IsFacingRight { get; set; }
+    public float Speed { get; set; }
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        Speed = _speed;
+        IsFacingRight = true;        
+
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
+        MoveProjectile();
+    }
+
+    public void MoveProjectile()
+    {
+        // testing
+        _movement = Direction * Speed * Time.fixedDeltaTime;
+        _rigidbody2D.MovePosition(_rigidbody2D.position + _movement);
+
+        Speed += _acceleration * Time.deltaTime;
+    }
+
+    public void FlipProjectile()
+    {
+        if (_spriteRenderer != null)
+        {
+            // flip to the opposite to what it was
+            _spriteRenderer.flipX = !_spriteRenderer.flipX;
+        }
+    }
+
+    public void SetDirection(Vector2 direction, Quaternion rotation, bool isFacingRight = true)
+    {
+        Direction = direction;
         
+        if (IsFacingRight != isFacingRight)
+        {
+            FlipProjectile();
+        }
+
+        transform.rotation = rotation;
     }
 }

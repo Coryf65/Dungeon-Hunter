53b44da6d11538dd2838a9c7f789dc36b07d14bb
Cory Fabian <Coryf65@users.noreply.github.com>
2022-11-16 14:39:51 -0600

    creating bullets using object pooling

---
+++ Assets/Scripts/Extras/ObjectPooler.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    [Tooltip("Prefab to create a pool for, ie: a bullet for a weapon")]
+    [SerializeField] private GameObject _objectPrefab;
+    [Range(0, 100)]
+    [SerializeField] private int _poolSize = 10;
+    [SerializeField] private bool _poolCanExpand = true;
+
+    private List<GameObject> _pooledObjects;
+    private GameObject _parentObject;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _parentObject = new(name: "Pooled Objects");
+        Refill();
+    }
+
+    public void Refill()
+    {
+        _pooledObjects = new();
+
+        for (int i = 0; i < _poolSize; i++)
+        {
+            AddObjectToPool();
+        }
+    }
+
+    public GameObject GetObjectFromPool()
+    {
+        for (int i = 0; i < _pooledObjects.Count; i++)
+        {
+            // find a disabled object to use
+            if (!_pooledObjects[i].activeInHierarchy)
+            {
+                return _pooledObjects[i];
+            }
+        }
+
+        if (_poolCanExpand)
+        {
+            // none left to use we need to expand
+            AddObjectToPool();
+        }
+        return null;
+    }
+
+    public GameObject AddObjectToPool()
+    {
+        GameObject gameObject = Instantiate(_objectPrefab);
+        gameObject.SetActive(false);
+        gameObject.transform.SetParent(_parentObject.transform, true);
+        _pooledObjects.Add(gameObject);
+
+        return gameObject;
+    }
+}

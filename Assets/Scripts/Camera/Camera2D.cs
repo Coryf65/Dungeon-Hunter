923b6154ef4c9896f191bbcae7de2272408c4a4c
Cory Fabian <Coryf65@users.noreply.github.com>
2022-11-16 16:53:57 -0600

    Seperated Camera Shaker

--- Assets/Scripts/Camera/Camera2D.cs
+++ Assets/Scripts/Camera/Camera2D.cs
@@ -7,11 +7,7 @@ public class Camera2D : MonoBehaviour
     [SerializeField] private Transform _targetTransform;
     [Header("Camera Settings")]
     [SerializeField] private Vector2 _offset = new(x: 0, y: 0); // default to center
-    [SerializeField] private CameraMode _cameraMode = CameraMode.Update;
-    [SerializeField] private bool _UseCameraShake = false;
-    [SerializeField] private float _shakeVibrato = 10f;
-    [SerializeField] private float _shakeRandomness = 0.1f;
-    [SerializeField] private float _shakeDuration = 0.01f;
+    [SerializeField] private CameraMode _cameraMode = CameraMode.Update;    
 
     private enum CameraMode
     {
@@ -53,47 +49,5 @@ public class Camera2D : MonoBehaviour
         );
 
         transform.position = desiredPosition;
-    }
-
-    public void ShakeCamera()
-    {
-        StartCoroutine(IShaker());
-    }
-
-    /// <summary>
-    /// Coroutine to shake the camera
-    /// </summary>
-    /// <returns></returns>
-    private IEnumerator IShaker()
-    {
-        Vector3 currentPosition = transform.position;
-
-        for (int i = 0; i < _shakeVibrato; i++)
-        {
-            Vector3 shakePosition = currentPosition + Random.onUnitSphere * _shakeRandomness;
-            yield return new WaitForSeconds(_shakeDuration);
-            transform.position = shakePosition;
-        }
-        yield return null;
-    }
-    private void OnShooting()
-    {
-        ShakeCamera();
-    }
-
-    private void OnEnable()
-    {
-        if (_UseCameraShake)
-        {
-            CharacterWeapon.OnStartShooting += OnShooting;
-        }
-    }
-
-    private void OnDisable()
-    {
-        if (_UseCameraShake)
-        {
-            CharacterWeapon.OnStartShooting -= OnShooting;
-        }
-    }
+    }         
 }
---
+++ Assets/Scripts/Camera/Camera2DShaker.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera2DShaker : MonoBehaviour
+{
+    [Header("Turn on/off the Camera Shaker")]
+    [SerializeField] private bool _UseCameraShake = true;
+    [Header("Camera2D Shaker Settings")]
+    [SerializeField] private float _shakeVibrato = 10f;
+    [SerializeField] private float _shakeRandomness = 0.1f;
+    [SerializeField] private float _shakeDuration = 0.01f;
+
+    public void ShakeCamera()
+    {
+        StartCoroutine(IShaker());
+    }
+
+    /// <summary>
+    /// Coroutine to shake the camera
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator IShaker()
+    {
+        Vector3 currentPosition = transform.position;
+
+        for (int i = 0; i < _shakeVibrato; i++)
+        {
+            Vector3 shakePosition = currentPosition + Random.onUnitSphere * _shakeRandomness;
+            yield return new WaitForSeconds(_shakeDuration);
+            transform.position = shakePosition;
+        }
+        yield return null;
+    }
+
+    private void OnShooting()
+    {
+        ShakeCamera();
+    }
+
+    private void OnEnable()
+    {
+        if (_UseCameraShake)
+        {
+            CharacterWeapon.OnStartShooting += OnShooting;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (_UseCameraShake)
+        {
+            CharacterWeapon.OnStartShooting -= OnShooting;
+        }
+    }
+}

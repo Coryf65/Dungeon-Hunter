3dadfb7f5295b73ed8ace1839a03872f6dedb334
Cory Fabian <Coryf65@users.noreply.github.com>
2022-11-16 20:44:36 -0600

    comparing the layers that we want to check for with the bullets

--- Assets/Scripts/Extras/ReturnToPool.cs
+++ Assets/Scripts/Extras/ReturnToPool.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class ReturnToPool : MonoBehaviour
 {
     [SerializeField] private float _lifetime = 2f; // seconds
+    [SerializeField] private LayerMask _objectMask;
 
     private Projectile _projectile;
 
@@ -23,6 +24,30 @@ public class ReturnToPool : MonoBehaviour
         }
 
         gameObject.SetActive(false);
+    }
+
+    /// <summary>
+    /// We have collided with something
+    /// </summary>
+    /// <param name="collidedWith">The GameObject that we have collided with</param>
+    private void OnTriggerEnter2D(Collider2D collidedWith)
+    {
+        if (CheckLayer(collidedWith.gameObject.layer, _objectMask))
+        {
+            Return();
+        }
+    }
+
+    /// <summary>
+    /// Compare the layer to another layer (objectMask)
+    /// </summary>
+    /// <param name="layer">Layer to compare with</param>
+    /// <param name="objectMask">Your layer that you are checking for</param>
+    /// <returns>t/f if the layers are the same</returns>
+    private bool CheckLayer(int layer, LayerMask objectMask)
+    {
+        // we have a comparison that returns true
+        return ((1 << layer) & objectMask) != 0;
     }
 
     /// <summary>

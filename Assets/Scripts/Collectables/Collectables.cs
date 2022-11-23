73a8951b122a38f5f612ef482e3687165a019516
Cory Fabian <Coryf65@users.noreply.github.com>
2022-11-22 18:59:07 -0600

    Creating a Pickup / collectables class

---
+++ Assets/Scripts/Collectables/Collectables.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+public class Collectables : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private bool _canDestroyItem = true;
+
+    private Character _character;
+    private GameObject _objectCollided;
+    private SpriteRenderer _spriteRenderer;
+    private Collider2D _collider2D;
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _collider2D = GetComponent<Collider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        _objectCollided = collision.gameObject;
+
+        // Check if it is a pickup
+        if (!IsAPickup(_objectCollided))
+            return;
+
+        HandlePickup();
+        PlayEffects();
+        CleanupObject(_canDestroyItem);
+    }
+
+    /// <summary>
+    /// Check if the gameobject is a Pickup, looks into GameObject's components for
+    /// a Character Component and having the CharacterType == Player.
+    /// </summary>
+    /// <param name="item">GameObject to check</param>
+    /// <returns>If the Item is a Pickup for the player</returns>
+    protected virtual bool IsAPickup(GameObject item)
+    {
+        bool isAPickup = false;
+        _character = item.GetComponent<Character>();
+
+        if (_character.CharacterType == Character.CharacterTypes.Player)
+        {
+            isAPickup = true;
+        }
+
+        return isAPickup;
+    }
+
+    protected virtual void HandlePickup()
+    {
+
+    }
+
+    protected virtual void PlayEffects()
+    {
+
+    }
+
+    /// <summary>
+    /// Handles GameObject Cleanup settings. If the object is set to 
+    /// Destroy it will remove. otherwise after pickup it hides the object.
+    /// </summary>
+    private void CleanupObject(bool destroyItem)
+    {
+        if (destroyItem)
+        {
+            Destroy(gameObject);
+        }
+
+        if (!destroyItem)
+        {
+            _spriteRenderer.enabled = false;
+            _collider2D.enabled = false;
+        }
+    }
+}
